8649777fb58d37409f98e77399bed70965260b49
Carboexco <126489763+Carboexco1@users.noreply.github.com>
2023-03-07 08:56:28 -0500

    Update cantHornos.js

--- js/cantHornos.js
+++ js/cantHornos.js
@@ -1,11 +1,12 @@
 function hornos() {
-    CantHorno();
+    CantHorno11();
+    CantHorno22();
 }
 
 
-function CantHorno() {
-var numPartes = 22;
-var partesHorno = document.getElementById("partes-horno");
+function CantHorno11() {
+var numPartes = 11;
+var partesHorno = document.getElementById("partes-horno11");
               
 for (var i = 1; i <= numPartes; i++) {
 var checkbox = document.createElement("input");
@@ -22,3 +23,25 @@ var br = document.createElement("br");
 partesHorno.appendChild(label);
 partesHorno.appendChild(br);
 }}
+
+
+function CantHorno22() {
+    var numPartes = 22;
+    var partesHorno = document.getElementById("partes-horno22");
+                  
+    for (var i = 12; i <= numPartes; i++) {
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.name = "parte-horno-" + i;
+    checkbox.value = "parte-horno-" + i;
+                  
+    var label = document.createElement("label");
+    label.appendChild(checkbox);
+    label.appendChild(document.createTextNode("Parte " + i));
+                  
+    var br = document.createElement("br");
+                  
+    partesHorno.appendChild(label);
+    partesHorno.appendChild(br);
+    }}
+    
