01e69ced387915575b1c9ceb6a7ac6beff5a5ca5
Carboexco <126489763+Carboexco1@users.noreply.github.com>
2023-03-07 10:36:04 -0500

    Update dashboard.js

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -38,8 +38,8 @@ google.charts.setOnLoadCallback(drawChart);
     function drawChart() {
         var data = google.visualization.arrayToDataTable([
             ['Year', 'Sales', 'Expenses', 'Profit'],
-            ['2014', 1000, 400, 200],
-            ['2015', 1170, 460, 250],
+            ['2014', 1000, 400, null],
+            ['2015', 1170, 460, null],
             ['2016', 660, 1120, 300],
             ['2017', 1030, 540, 350]
           ]);
