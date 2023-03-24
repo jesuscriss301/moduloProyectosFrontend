88e41a65318102ccbbc3a14746da3f28a8e51626
Carboexco <126489763+Carboexco1@users.noreply.github.com>
2023-03-24 17:03:21 -0500

    aggdiseño.js

---
+++ js/aggDiseño.js
@@ -0,0 +1,9 @@
+const formDiseño =(event) => {
+    event.preventDefault();
+const datos =new FormData(event.target);
+console.log(datos);
+
+const datosCompletos = Object.fromEntries(datos.entries());
+
+console.log(datosCompletos);
+}
