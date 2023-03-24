740c11b7d27438d26144e6f9df23a74cada7dc49
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-03-24 15:59:28 -0500

    busqueda por nombre

--- JS/proyectos.js
+++ JS/proyectos.js
@@ -1,22 +1,8 @@
-function cargarTabla() {
+function cargarTablaProyectos() {
   //Tabla();
   consultaNombreProyecto()
 }
-function Tabla(busqueda) {
-google.charts.load('current', {'packages':['table']});
-google.charts.setOnLoadCallback(proyectos(busqueda));
-function proyectos(busqueda) {
-fetch('http://sistemas:8080/proyectos/tablaProyectos/'+busqueda+"/")
-  .then(response => response.json())
-  .then(data => drawTable(data))
-  .catch(error => console.log(error));
-}
-function drawTable(info) {
-  
-var data = new google.visualization.arrayToDataTable(info);
-var table = new google.visualization.Table(document.getElementById('table_div'));
-table.draw(data, {showRowNumber: false, width: '100%', height: '100%'});
-}}
+
 function consultaNombreProyecto() {
   const urlParams = new URLSearchParams(window.location.search);
   const queryParam = urlParams.get('nombreProyecto');
@@ -24,9 +10,29 @@ function consultaNombreProyecto() {
     Tabla(queryParam)
   }else{
     Tabla("*");
+    console.log("*");
   }
   console.log(queryParam); // "JavaScript"  
 }
+
+function Tabla(busqueda) {
+  google.charts.load('current', {'packages':['table']});
+  google.charts.setOnLoadCallback(proyectos(busqueda));
+}
+function proyectos(busqueda) {
+  fetch('http://sistemas:8080/proyectos/tablaProyectos/'+busqueda+"/")
+    .then(response => response.json())
+    .then(data => drawTable(data))
+    .catch(error => console.log(error));
+}
+
+function drawTable(info) {
+  
+  var data = new google.visualization.arrayToDataTable(info);
+  var table = new google.visualization.Table(document.getElementById('table_div'));
+  table.draw(data, {showRowNumber: false, width: '100%', height: '100%'});
+}
+
 function alerta() {
   const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
   alertPlaceholder.setAttribute("class", "m-3");
