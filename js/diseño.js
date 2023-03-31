e8b7dfe7a040c8f36ecd284cd7c1850115101621
Carboexco <126489763+Carboexco1@users.noreply.github.com>
2023-03-31 14:05:38 -0500

    ajuste tabla diseño

---
+++ js/diseño.js
@@ -0,0 +1,47 @@
+const URL_BASE = "http://sistemas:8080";
+
+function cargar() {
+    const urlParams = new URLSearchParams(window.location.search);
+    let queryParam = urlParams.get('nombreProyecto');
+    //console.log(queryParam);
+    if (queryParam!=null && queryParam!="") {
+    cargarProyecto(queryParam);
+    }
+}
+
+async function cargarProyecto(id) {
+    fetch(`${URL_BASE}/proyectos/${id}`)
+    .then(response => response.json())
+    .then(data => {
+        tablaInfo(data);
+        responsable(data.id);
+    })
+    .catch(error => console.log(error));
+}
+
+async function responsable(proyecto) {
+        
+    const responsable = document.getElementById("responsable");
+
+    fetch(`${URL_BASE}/proyectoPersonas/proyecto/${proyecto}/2`)
+    .then(response => response.json())
+    .then(data => {
+        const idPersonas = data.map(item => item.id.persona);
+        const rta = idPersonas.join(", ");
+        responsable.textContent = rta;
+    })
+    .catch(error => console.log(error));
+}
+
+function tablaInfo(data) {
+    
+    const codigo = document.getElementById("codigoProyecto");
+    const nombre = document.getElementById("nombreProyecto");
+    const tipo= document.getElementById("tipoProyecto");
+    
+    codigo.textContent=data.id;
+    nombre.textContent=data.nombreProyecto;
+    tipo.textContent=data.idTipoProyecto.nombre;
+
+}
+
